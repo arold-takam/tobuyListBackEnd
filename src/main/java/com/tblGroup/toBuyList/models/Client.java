f030d892c07a81aed832609de66a474c1d61aa27
steve <nguenosteve3@gmail.com>
2025-08-19 08:06:14 +0100

    my last work

--- src/main/java/com/tblGroup/toBuyList/models/Client.java
+++ src/main/java/com/tblGroup/toBuyList/models/Client.java
@@ -40,7 +40,7 @@ public class Client {
 		this.mail = mail;
 		this.password = password;
 		this.moneyAccountList = moneyAccountList;
-                this.wallet = wallet;
+		this.wallet = wallet;
     }
 
 //	------------------------------------------------------------------------------------------------------------
@@ -81,6 +81,7 @@ public class Client {
 		this.moneyAccountList = moneyAccountList;
 	}
 
+
     public Wallet getWallet() {
         return wallet;
     }
@@ -88,5 +89,4 @@ public class Client {
     public void setWallet(Wallet wallet) {
         this.wallet = wallet;
     }
-    
 }
