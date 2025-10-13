de70faafb0bb0101a3dec230ca889c575420308b
steve <nguenosteve3@gmail.com>
2025-10-13 08:25:10 +0100

    modification the moneyAccountController file

--- src/main/java/com/tblGroup/toBuyList/controllers/MoneyAccountController.java
+++ src/main/java/com/tblGroup/toBuyList/controllers/MoneyAccountController.java
@@ -93,7 +93,6 @@ public class MoneyAccountController {
 	}
 	
 	
-	
 	//	----------------------------------TRANSACTIONS MANAGEMENT----------------------------------------------------------
 	@PutMapping(path = "/deposit/{mAccountID}",
 		produces = APPLICATION_JSON_VALUE)
